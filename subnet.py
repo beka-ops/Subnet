849aec43f66659d3060ad283403251da89916fcb
beka-ops <rh4207@nyu.edu>
2025-03-26 21:48:48 -0400

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 6761
+
+#problem 1
+p1Network = '127.82.245.0'
+p1Broadcast = '127.82.245.255'
+p1First = '127.82.245.1'
+p1Last =  '127.82.245.254'
+
+#problem 2
+p2Network = '38.67.207.128'
+p2Broadcast = '38.67.207.143'
+p2First = '38.67.207.129'
+p2Last = '38.67.207.142'
+
+#problem 3
+p3Network = '205.52.169.128'
+p3Broadcast = '205.52.169.255'
+p3First = '205.52.169.129'
+p3Last = '205.52.169.254'
+
+#problem 4
+p4Network = '116.37.130.208'
+p4Broadcast = '116.37.130.215'
+p4First = '116.37.130.209'
+p4Last = '116.37.130.214'
+
+#problem 5
+p5Network = '27.22.92.0'
+p5Broadcast = '27.22.92.63'
+p5First = '27.22.92.1'
+p5Last = '27.22.92.62'
+
+#problem 6
+p6Network = '197.7.54.0'
+p6Broadcast = '197.7.54.255'
+p6First = '197.7.54.1'
+p6Last =  '197.7.54.254'
+
+#problem 7
+p7Network = '105.249.16.64'
+p7Broadcast = '105.249.16.95'
+p7First = '105.249.16.65'
+p7Last = '105.249.16.94'
+
+#problem 8
+p8Network = '16.234.233.128'
+p8Broadcast = '16.234.233.255'
+p8First = '16.234.233.129'
+p8Last = '16.234.233.254'
+
+#problem 9
+p9Network = '183.219.195.160'
+p9Broadcast = '183.219.195.175'
+p9First = '183.219.195.161'
+p9Last = '183.219.195.174'
+
+#problem 10
+p10Network = '94.204.157.192'
+p10Broadcast = '94.204.157.255'
+p10First = '94.204.157.193'
+p10Last = '94.204.157.254'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
